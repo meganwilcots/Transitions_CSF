1b1db0832819cd1a77ba663f29e19aadbdc54c8a
Megan Wilcots <megan.wilcots@gmail.com>
2022-11-16 11:02:24 -0800

    Added log-log  model

--- knz-data-exploration.R
+++ knz-data-exploration.R
@@ -79,8 +79,10 @@ knz_standardized <- knz_final %>%
 library(nlme)
 mod_tp <- lme(NPP_stand ~ MAP_stand + temp_stand, random = ~1|uniqueID, data = knz_standardized)
 mod_spei <- lme(NPP_stand ~ SPEI_6m, random = ~1|uniqueID, data = knz_standardized)
+knz_standardized$ANPP01 <- knz_standardized$ANPP+0.1 ## there's a zero and it's messing up the model
+mod_tp1 <- lme(log(ANPP01) ~ log(max_t) + log(MAP_mm), random = ~1|uniqueID, data = knz_standardized)
 
-summary(mod_tp)
+summary(mod_tp1)
 summary(mod_spei)
 library(vegan)
 
@@ -107,12 +109,12 @@ ggplot(knz_standardized, aes(x = MAP_stand, y = NPP_stand)) +
 ### test w/ data in the google sheet
 
 test_data <- tibble(site = c("CDR", "KBS", "KNZ"),
-                    coef_temp = c(-0.033, -0.102, 0.090),
-                    se_temp = c(0.031, 0.066, 0.014),
-                    coef_precip = c(0.115, 0.036, 0.186),
-                    se_precip = c(0.031, 0.066, 0.013),
-                    coef_spei = c(-0.098, -0.044, 0.083),
-                    se_spei = c(0.033, 0.066, 0.013),
+                    coef_temp = c(-0.033, 0.082, 0.090),
+                    se_temp = c(0.031, 0.065, 0.014),
+                    coef_precip = c(0.115, 0.249, 0.186),
+                    se_precip = c(0.031, 0.065, 0.013),
+                    coef_spei = c(-0.098, 0.141, 0.083),
+                    se_spei = c(0.033, 0.068, 0.013),
                     MAP = c(831.052, 905.7, 796.1),
                     max_temp = c(12.5, 34.8, 19.3),
                     mean_spei = c(-0.616, 0.207, 0.015))
